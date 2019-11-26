f098c685845e3c7f47131410625a586c8464dc05
Catalin <catalin@fsbtech.com>
2019-11-26 14:46:39 +0000

    added new data to Bb Model

--- app/components/views/betslipItemView.js
+++ app/components/views/betslipItemView.js
@@ -2,7 +2,7 @@ import { View } from 'backbone.marionette';
 import BestlipModel from '../models/betslip_model';
 
 const BetslipItemView = View.extend({
-  el: '#betslip_item',
+  tagName: "li",
   template: _.template(`<div>
                           <h6> <%= name %> </h6>
                           <button disabled="<%= active %>"> <%= price %> </button>
